d7177c9caa5df4fb29577a6c34e8baf645204398
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2025-06-03 22:48:26 +0600

    Create whatscrapper.py

---
+++ whatscrapper.py
@@ -0,0 +1,8 @@
+import os, platform, time, sys
+os.system('git pull')
+print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m Checking For Update...')
+os.system('git pull --quiet 2>/dev/null')
+bit = platform.architecture()[0]
+if bit == '64bit':
+ print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m 64Bit Found')
+ import whatscrapper
