7c726d27385d51f27a3e85949dbca9a22296cddf
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2025-06-04 20:50:28 +0600

    Update scrapped.py

--- scrapped.py
+++ scrapped.py
@@ -5,4 +5,4 @@ os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
  print('\033[1;31m[\x1b[38;5;48m●\033[1;31m]\x1b[38;5;48m 64Bit Found')
- import sc
+ import what
