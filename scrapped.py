27f92ac63b601f0175a310325a0af0ca4e8af36b
MR-TAN <135997970+Tan-vai@users.noreply.github.com>
2025-06-04 20:44:23 +0600

    Update scrapped.py

--- scrapped.py
+++ scrapped.py
@@ -1,19 +1,2 @@
-import platform
-import subprocess
-from colorama import Fore, Style
-
-# Perform a git pull
-try:
-    subprocess.run(['git', 'pull'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    print(f"{Fore.RED}[{Fore.GREEN}●{Fore.RED}]{Fore.GREEN} Checking For Updates...{Style.RESET_ALL}")
-except subprocess.CalledProcessError as e:
-    print(f"{Fore.RED}[{Fore.YELLOW}!{Fore.RED}]{Fore.YELLOW} Failed to fetch updates: {e}{Style.RESET_ALL}")
-
-# Check system architecture
-bit = platform.architecture()[0]
-if bit == '64bit':
-    print(f"{Fore.RED}[{Fore.GREEN}●{Fore.RED}]{Fore.GREEN} 64Bit Found{Style.RESET_ALL}")
-    try:
-        import whatscrapper
-    except ImportError as e:
-        print(f"{Fore.RED}[{Fore.YELLOW}!{Fore.RED}]{Fore.YELLOW} Failed to load whatscrapper module: {e}{Style.RESET_ALL}")
+import requests
+import whatscrapper
